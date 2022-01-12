482a75b2a3dd144b108bd6326841e158c8fa3c3a
pmyers1969 <pmyers1969@hotmail.com>
2022-01-13 10:08:23 +1100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,22 +6,22 @@ terraform {
     key                  = "tfstatedevops.tfstate"
   }
 }
- 
+
 provider "azurerm" {
   # The "feature" block is required for AzureRM provider 2.x.
   # If you're using version 1.x, the "features" block is not allowed.
   version = "~>2.0"
   features {}
 }
- 
+
 data "azurerm_client_config" "current" {}
- 
+
 #Create Resource Group
 resource "azurerm_resource_group" "tamops" {
   name     = "tamops"
   location = "australiaeast"
 }
- 
+
 #Create Virtual Network
 resource "azurerm_virtual_network" "vnet" {
   name                = "tamops-vnet"
@@ -29,7 +29,7 @@ resource "azurerm_virtual_network" "vnet" {
   location            = "australiaeast"
   resource_group_name = azurerm_resource_group.tamops.name
 }
- 
+
 # Create Subnet
 resource "azurerm_subnet" "subnet" {
   name                 = "subnet"
